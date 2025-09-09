5765f22b22a9d3c91a76a9b0630b055cba2d51fd
Zirjishere <b2029zhfmendaro@pshs.edu.ph>
2025-09-09 15:31:00 +0800

    changes

--- test.js
+++ test.js
@@ -13,3 +13,12 @@ do {
     console.log(i);
     i++;
 } while (i <= 3);
+
+grades_arr = [99, 95, 100, 98, 96]
+
+arr2 = grades_arr;
+arr2[0] = 1000;
+for (let i = 0; i < grades_arr.length; i++) {
+    console.log(grades_arr[i]);
+}
+console.log(arr2[0]);
