abccb5ce3f4b80388c8b3aad0be1e6b9a46d711d
Zirjishere <b2029zhfmendaro@pshs.edu.ph>
2025-09-09 15:22:05 +0800

    test js changes

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
-for(let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 3; i++) {
     console.log(i);
 }
+
+var i= 1;
+while (i <= 3) {
+    console.log(i);
+    i++;
+}
+
+i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 3);
